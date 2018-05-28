b769f24c0a04ca51b074a75b4f3cedda0aac520d
foxniver <32783730+foxniver@users.noreply.github.com>
2018-05-27 19:29:30 -0500

    Adding style sheets.

--- project1/projects/project01/madlib.php
+++ project1/projects/project01/madlib.php
@@ -15,12 +15,17 @@
  $insertQuery = "INSERT INTO mad_libs (noun, verb, adjective, adverb, story) " .
                 "VALUES ('$noun', '$verb', '$adjective', '$adverb', '$story')";
 
- $returnedStory = "select story from mad_libs where noun = 'vehicle'";
 
- $result = $connection->query($returnedStory);
+
+
  mysqli_query($connection, $insertQuery);
- mysqli_close($connection);
+ $returnedStory = "select noun, verb, adjective, adverb, story from mad_libs order by timestamp desc";
+ $result = $connection->query($returnedStory);
 
+ echo "<!DOCTYPE html>";
+ echo "<html>";
+ echo "<head><title>Result</title><link rel=\"stylesheet\" type=\"text/css\" href=\"index.css\"></head>";
+ echo "<body>";
  echo "Enter a  noun: $noun<br/>" .
       "Enter a verb: $verb<br/>" .
       "Enter an adjective: $adjective<br/>" .
@@ -29,11 +34,20 @@
 
  if ( $result->num_rows > 0)
  {
+     echo "<table><tr><th>noun</th><th>verb</th><th>adjective</th><th>adverb</th><th>story</th></tr>";
      while($row = $result->fetch_assoc())
      {
-        echo "story: " . $row["story"];
-
+        echo "<tr>"
+            ."<td>".$row["noun"]."</td>"
+            ."<td>".$row["verb"]."</td>"
+            ."<td>".$row["adjective"]."</td>"
+            ."<td>".$row["adverb"]."</td>"
+            ."<td>".$row["story"]."</td>"
+        ."</tr>";
      }
+     echo "</table>";
  }
-
+echo "</body>";
+echo "</html>";
+ mysqli_close($connection);
 ?>
